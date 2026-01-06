13b8e127552aca7cfbdd0b62fbd12975064f213a
Juicebox_joe <DuartemJoseph@gmail.com>
2026-01-06 15:38:14 -0800

    new fix

--- myfirstapi/obj/Debug/net9.0/myfirstapi.AssemblyInfo.cs
+++ myfirstapi/obj/Debug/net9.0/myfirstapi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("myfirstapi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+01efbeda1786aa582942a9e9313e9980183214ee")]
 [assembly: System.Reflection.AssemblyProductAttribute("myfirstapi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("myfirstapi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
